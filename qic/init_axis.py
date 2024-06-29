Fixed sign in construction of Frenet in phi grid (benchmarked cross-sections against pyQSC)

qic/init_axis.py
@@ -85,8 +85,9 @@ def init_axis(self, omn_complete = True):
         # Evaluate the axis in cylindrical coordinates (note that it assumes the curve closes; could have
         # some discontinuity, might need an additional optimisation to close the curve)
         # It modifies phi, nu and the Frenet geometry accordingly, using varphi as regular grid
+        flag_half = (np.mod(self.helicity_in, 1) == 0.5) # If half helicity, take into account the flip of sign
         flag_func = True if ("function_ell" in self.curvature_in) else False
-        _ = invert_frenet_axis(self, self.curvature, self.torsion, self.ell, self.varphi, full_axis = True, func = flag_func)
+        _ = invert_frenet_axis(self, self.curvature, self.torsion, self.ell, self.varphi, full_axis = True, func = flag_func, flip = flag_half)
         
         # Obtain axis description as Fourier components : important for output to VMEC (at least approximately)
         ntor = 10
@@ -112,7 +113,7 @@ def init_axis(self, omn_complete = True):
         d3_l_d_phi3 = np.matmul(self.d_d_phi, d2_l_d_phi2)
 
         # Final value for B0
-        Bbar = 1 # self.spsi * np.mean(self.B0)
+        Bbar = self.spsi * np.mean(B0)
         self.B0_spline = self.convert_to_spline(B0) # splines are in phi
 
         # Final value for G0
@@ -121,7 +122,7 @@ def init_axis(self, omn_complete = True):
 
         ## Evaluation of d ##
         if not self.omn:
-            self.d_bar = self.etabar / curvature
+            self.d_bar = self.etabar / self.curvature
         else:
             d = np.zeros(self.nphi)
             if isinstance(self.d_in, dict):
@@ -136,7 +137,7 @@ def init_axis(self, omn_complete = True):
             # If only d_bar as an input, simply use that
             self.d_bar = dbar
         else:
-            self.d_bar = self.d / curvature
+            self.d_bar = self.d / self.curvature
 
         # Define auxiliary quantity
         self.etabar_squared_over_curvature_squared = (B0 / Bbar ) * self.d_bar**2
@@ -400,7 +401,7 @@ def init_axis(self, omn_complete = True):
         ###############################################
         if self.omn == False:
             # Compute G0 for QS field: in here B0 is a scalar (this was done in __init__)
-            G0 = self.sG * np.trapz(np.append(B0 * d_l_d_phi,B0[0] * d_l_d_phi[0]), \
+            G0 = self.sG * np.trapz(np.append(self.B0 * d_l_d_phi, self.B0[0] * d_l_d_phi[0]), \
                                     np.append(self.phi,self.phi[0]+2*np.pi/self.nfp)) / (2*np.pi/self.nfp)
 
             abs_G0_over_B0 = self.sG*G0/self.B0

qic/reverse_frenet_serret.py
@@ -10,9 +10,6 @@ from scipy.integrate import solve_ivp, cumulative_trapezoid, quad
 from scipy.interpolate import PchipInterpolator, make_interp_spline
 from sklearn.decomposition import PCA
 from scipy.spatial.transform import Rotation as R
-from qic.fourier_interpolation import fourier_interpolation
-from qic.spectral_diff_matrix import construct_periodic_diff_matrix
-
 
 logger = logging.getLogger(__name__)
 
@@ -323,7 +320,8 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
     aligned_B = np.einsum('ji,ki->kj', rotation_matrix, B)
 
     # Check whether the sense of the axis is in the positive cylindrical angle
-    sense_axis = (phi[1] % (2*np.pi)) - (phi[0] % (2*np.pi))
+    phi = np.unwrap(phi)
+    sense_axis = phi[1] - phi[0]
 
     if sense_axis < 0 or sense_axis > np.pi: # Care for potential sudden jump
         # Change coordinates accordingly
@@ -338,7 +336,7 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
         aligned_position[:,2] *= -1
         aligned_T[:,2] *= -1
         aligned_N[:,2] *= -1
-        aligned_B[:,2] *= -1
+        aligned_B[:,2] *= -1   
     
     # Need to translate aligned vectors to cylindrical coordinates
     def change_vector_to_cylindrical(phi, vector):
@@ -359,8 +357,9 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
     aligned_B = change_vector_to_cylindrical(phi, aligned_B)
 
     # Redefine the cylindrical angle so that the first point is phi = 0 (the cylindrical representation should not change
-    phi = np.unwrap(phi)
-    phi = (phi-phi[0])/phi[-1]*2*np.pi
+    assert np.sign(phi[-1]-phi[0]) > 0
+    print('Mismatch in phi_cyl: ', phi[-1]-2*np.pi)
+    phi = (phi-phi[0])/(phi[-1]-phi[0])*2*np.pi
 
     ################
     # SPLINES OF r #
@@ -402,8 +401,6 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
         kappa = kappa(ell)
         tau = tau(ell)
         
-    nu_func = make_spline(varphi, varphi - phi, periodic = True)
-
     # Due to sign, for half helicities, the configurations have sign flips in normal/binormal. We consider a continuous frame within 
     # a whole 2pi turn, and will be discontinuous at phi = 0. Keep it in vylindrical phi.
     self.normal_R_spline = make_spline(phi, aligned_N[:,0], half = flag_half)
@@ -429,6 +426,7 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
     ##############################
     # Do nothing to the curvature and torsion: these are assumed to be given in varphi grid
     # Evaluate phi
+    nu_func = make_spline(varphi, varphi - phi, periodic = True)
     self.nu = nu_func(varphi_in)
     phi_out = varphi_in - self.nu
     self.phi = phi_out
@@ -470,7 +468,7 @@ def invert_frenet_axis(self, curvature, torsion, ell, varphi, plot = False, full
         ax.plot(origin[0],origin[1],origin[2], label='Curve')
 
         # Plotting normal and binormal vectors as arrows
-        stp = 50
+        stp = int(nphi/10)
         num = int(nphi/stp)
 
         for i in range(num):